1f7f04fbfeb51b53c331497bbd3fa569a3e28479
m-arch <themouallem@gmail.com>
2014-10-14 12:04:19 +0300

    fixed relative urls

--- logic/task.lisp
+++ logic/task.lisp
@@ -15,7 +15,7 @@
   (let ((task (select-task-by-id (data-param "id")))
 	(parent-tasks (select-tasks-id-and-title)))
     (list
-     :post-url (genurl 'task-post)
+     :post-url "/task"
      :action action
      :page-title (if task (title task) "New Task")
      :task task
--- utilities/task-utils.lisp
+++ utilities/task-utils.lisp
@@ -12,7 +12,7 @@
 			       "delay")
 			      (t
 			       "pending"))
-			    (genurl 'task-get :action "edit" :id  (id task)) (title task) 
+			    (format nil "/task/edit?id=~A" (id task)) (title task) 
 			    (if (description task) 
 				(description task) 
 				"no description")
@@ -20,7 +20,7 @@
 			    (format nil "submitTaskEdit(\"~A\",\"~A\",\"~A\",\"~A\");return false;" (genurl 'task-post) "delete" (genurl 'tasks-get) (id task))
 			    (if (not (done-p task))
 				(format nil "<button onclick='submitTaskEdit(\"~A\", \"~A\", \"~A\", \"~A\"); return false;'>done</button>" 
-					(genurl 'task-post) "complete" (genurl 'tasks-get) (id task))
+					"/task" "complete" "/tasks" (id task))
 				"")
 			    (fill-tasks-html-from-parent-tasks (child-tasks task) :is-child-p t))))
     (if (and (not is-child-p) (string= "" html))
